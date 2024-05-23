dba4b9a5de61abf97273b8154e6855daa589248a
emmachen12 <emc1207pi@gmail.com>
2024-05-23 14:59:38 -0400

    g

--- Robot2.java
+++ Robot2.java
@@ -5,8 +5,31 @@ public class Robot2{
 
     
         Robot izzy = new Robot ();
-        izzy.mouseMove(0,0);
+        izzy.setAutoDelay(100);
+
+        izzy.mouseMove(0,1080);
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        izzy.mouseMove (200,920);
+        izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        izzy.mouseMove (1230,500);
+        izzy.setAutoDelay(900);
+        izzy.mouseMove (1231,500);
+        izzy.setAutoDelay(1300);
+        izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+       
+        izzy.mouseMove (1220,850);
+        izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        izzy.mouseMove (900,850);
+        izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
     
 }
