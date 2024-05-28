543d05eb5a8e392814c4d729af8cb9f78de0ca3b
emmachen12 <emc1207pi@gmail.com>
2024-05-28 14:50:58 -0400

    f

--- Robot2.java
+++ Robot2.java
@@ -1,4 +1,12 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File; 
+
+
 import java.awt.event.InputEvent;
 public class Robot2{
     public static void main (String[]args) throws Exception{
@@ -11,12 +19,12 @@ public class Robot2{
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-        izzy.mouseMove (200,920);
+        izzy.mouseMove (200,735);
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
         izzy.mouseMove (1230,500);
-        izzy.setAutoDelay(900);
+        izzy.setAutoDelay(1200);
         izzy.mouseMove (1231,500);
         izzy.setAutoDelay(1300);
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
@@ -27,9 +35,22 @@ public class Robot2{
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-        izzy.mouseMove (900,850);
+        izzy.mouseMove (800,150);
         izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        izzy.mouseMove (800,260);
+        izzy.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        izzy.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle bounds = new Rectangle(screenSize);
+        BufferedImage image= izzy.createScreenCapture(bounds); 
+        File file = new File("myScreenShot.png" );
+        ImageIO.write(image,"png", file);
+        System.out.println("A screenshot is captured to" + file.getPath()); 
+
+
     }
     
 }
